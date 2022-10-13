5f66c6fe47e951ff47780afe43417e3d1549d103
tarannum <tarannumara0198@gmail.com>
2022-10-13 15:10:33 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -1,6 +1,6 @@
 import os
 import yaml
-import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -193,4 +193,3 @@ START_BLOCK_FOR_SCANNER = config.get('START_BLOCK_FOR_SCANNER')
 ENDPOINT = config.get('ENDPOINT')
 SCANNER_SLEEP_IN_SECONDS = config.get('SCANNER_SLEEP_IN_SECONDS')
 MIN_BLOCKS_FOR_SCANNER = config.get('MIN_BLOCKS_FOR_SCANNER')
-django_heroku.settings(locals())
