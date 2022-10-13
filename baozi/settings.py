6a3bd57113b76f7a75909022d18669e5522c9af2
tarannum <tarannumara0198@gmail.com>
2022-10-13 17:53:08 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -37,7 +37,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app/pools",   
+    "https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app/pools", 
+    "*",
+    '*',  
 ]
 
 ROOT_URLCONF = 'baozi.urls'
