84ef6396121eea478902dfc4bfb074af5b3de571
tarannum <tarannumara0198@gmail.com>
2022-10-13 14:45:41 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+IS_HEROKU = "DYNO" in os.environ
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -34,7 +36,10 @@ MIDDLEWARE = [
 ]
 
 
-ALLOWED_HOSTS = ['*']
+if IS_HEROKU:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
 
 ROOT_URLCONF = 'baozi.urls'
 
