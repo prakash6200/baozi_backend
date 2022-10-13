4cf02fa7bdcc688990f492c5cfc23d23b9aee8e4
tarannum <tarannumara0198@gmail.com>
2022-10-13 17:05:41 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'core',
     'rest_framework',
     'drf_yasg',
     'baozi.networks',
