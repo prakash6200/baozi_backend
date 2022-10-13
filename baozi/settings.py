463b042ebb520c3d3750ee97a63efaef4cd7ac96
tarannum <tarannumara0198@gmail.com>
2022-10-13 16:44:19 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -39,10 +39,19 @@ MIDDLEWARE = [
 # ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "https://baozi-swap-backend.herokuapp.com",
-    "baozi-swap-backend.herokuapp.com",
+    "https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app",
     "http://localhost:8080",
     "http://localhost:3000",
     "http://127.0.0.1:9000",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 
 ROOT_URLCONF = 'baozi.urls'
