dbea2d6d79087024265a2037d87bed06ed683876
tarannum <tarannumara0198@gmail.com>
2022-10-13 18:00:58 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -37,6 +37,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "https://baozi-swap-backend.herokuapp.com",
+    'https://baozi-swap-backend.herokuapp.com',
     "https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app", 
     'https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app', 
     "*",
