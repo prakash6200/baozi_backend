4884a4f38cbaa012b165c466a93e1217023d1a9d
tarannum <tarannumara0198@gmail.com>
2022-10-13 16:25:31 +0530

    make it better

--- baozi/settings.py
+++ baozi/settings.py
@@ -36,8 +36,9 @@ MIDDLEWARE = [
 
 # ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    "*",
     "https://baozi-swap-backend.herokuapp.com", 
+    "https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app/",
+    "https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app/pools"
 ]
 
 ROOT_URLCONF = 'baozi.urls'
