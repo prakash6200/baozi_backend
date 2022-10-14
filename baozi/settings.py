a2f0389c8a028d586a4435238804ea6622814a1a
tarannum <tarannumara0198@gmail.com>
2022-10-14 15:27:49 +0530

    cors added

--- baozi/settings.py
+++ baozi/settings.py
@@ -39,13 +39,6 @@ INSTALLED_APPS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-]
-CORS_ALLOWED_ORIGINS = [
-    'https://6347e9e149449816f45b5c92--lucky-pasca-98c953.netlify.app/',
-]
-
 
 ROOT_URLCONF = 'baozi.urls'
 
