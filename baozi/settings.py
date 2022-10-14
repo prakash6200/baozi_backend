f0d1cc7f175ba7df0b8224d680af158acf60d1a3
tarannum <tarannumara0198@gmail.com>
2022-10-14 15:36:52 +0530

    cors added

--- baozi/settings.py
+++ baozi/settings.py
@@ -7,6 +7,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Application definition
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOW_HEADERS = ['*']
+CSRF_TRUSTED_ORIGINS = ['*']
+CORS_ALLOW_CREDENTIALS = True
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
