9cef2be9603d061d5754bd2289da448b9bfbbbdb
tarannum <tarannumara0198@gmail.com>
2022-10-14 15:24:00 +0530

    cors added

--- baozi/settings.py
+++ baozi/settings.py
@@ -8,7 +8,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Application definition
 
-CORS_ORIGIN_ALLOW_ALL = '*'
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+]
 
 INSTALLED_APPS = [
     'corsheaders',
@@ -28,17 +38,6 @@ INSTALLED_APPS = [
     
 ]
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-]
 
 CORS_ALLOWED_ORIGINS = [
     '*',
